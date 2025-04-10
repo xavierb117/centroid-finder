f6ed5e8850acfc345e20682ad4fb59a2b9f274cc
Auberon López <auberonedu@gmail.com>
2025-04-10 08:59:43 -0700

    Add needed instance variables to binarizer and implement App

--- src/DistanceImageBinarizer.java
+++ src/DistanceImageBinarizer.java
@@ -4,34 +4,41 @@ import java.awt.image.BufferedImage;
  * An implementation of the ImageBinarizer interface that uses color distance
  * to determine whether each pixel should be black or white in the binary image.
  * 
- * The binarization is based on the distance between a pixel's color and a reference color.
+ * The binarization is based on the Euclidean distance between a pixel's color and a reference target color.
  * If the distance is less than the threshold, the pixel is considered white (1);
  * otherwise, it is considered black (0).
  * 
- * The color distance is computed using a provided ColorDistanceFinder, which defines how
- * to compare two colors numerically.
+ * The color distance is computed using a provided ColorDistanceFinder, which defines how to compare two colors numerically.
+ * The targetColor is represented as a 24-bit RGB integer in the form 0xRRGGBB.
  */
 public class DistanceImageBinarizer implements ImageBinarizer {
     private final ColorDistanceFinder distanceFinder;
+    private final int threshold;
+    private final int targetColor;
 
     /**
-     * Constructs a DistanceImageBinarizer using the given ColorDistanceFinder and threshold.
+     * Constructs a DistanceImageBinarizer using the given ColorDistanceFinder,
+     * target color, and threshold.
      * 
-     * The distanceFinder is used to compute the distance between a pixel color and a reference color.
+     * The distanceFinder is used to compute the Euclidean distance between a pixel's color and the target color.
+     * The targetColor is represented as a 24-bit hex RGB integer (0xRRGGBB).
      * The threshold determines the cutoff for binarization: pixels with distances less than
      * the threshold are marked white, and others are marked black.
      *
      * @param distanceFinder an object that computes the distance between two colors
+     * @param targetColor the reference color as a 24-bit hex RGB integer (0xRRGGBB)
      * @param threshold the distance threshold used to decide whether a pixel is white or black
      */
-    public DistanceImageBinarizer(ColorDistanceFinder distanceFinder, int threshold) {
+    public DistanceImageBinarizer(ColorDistanceFinder distanceFinder, int targetColor, int threshold) {
         this.distanceFinder = distanceFinder;
+        this.targetColor = targetColor;
+        this.threshold = threshold;
     }
 
     /**
      * Converts the given BufferedImage into a binary 2D array using color distance and a threshold.
      * Each entry in the returned array is either 0 or 1, representing a black or white pixel.
-     * A pixel is white (1) if its distance to the reference color is less than the threshold.
+     * A pixel is white (1) if its Euclidean distance to the target color is less than the threshold.
      *
      * @param image the input RGB BufferedImage
      * @return a 2D binary array where 1 represents white and 0 represents black
@@ -44,7 +51,7 @@ public class DistanceImageBinarizer implements ImageBinarizer {
     /**
      * Converts a binary 2D array into a BufferedImage.
      * Each value should be 0 (black) or 1 (white).
-     * Black is encoded as x000000 and white as xFFFFFF.
+     * Black pixels are encoded as 0x000000 and white pixels as 0xFFFFFF.
      *
      * @param image a 2D array of 0s and 1s representing the binary image
      * @return a BufferedImage where black and white pixels are represented with standard RGB hex values
