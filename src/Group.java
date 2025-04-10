e50e12a1020f19a84e5b040ee26b127dff88931c
Auberon López <auberonedu@gmail.com>
2025-04-10 08:02:42 -0700

    Added functionality to Group and renamed findConnectedGroups

--- src/Group.java
+++ src/Group.java
@@ -1,3 +1,18 @@
-public record Group(int size, Coordinate centroid) {
-    
+public record Group(int size, Coordinate centroid) implements Comparable<Group> {
+    @Override
+    public int compareTo(Group other) {
+        int comp = Integer.compare(this.size(), other.size());
+        if (comp != 0) {
+            return comp;
+        }
+        comp = Integer.compare(this.centroid().x(), other.centroid().x());
+        if (comp != 0) {
+            return comp;
+        }
+        return Integer.compare(this.centroid().y(), other.centroid().y());
+    }
+
+    public String toCsvRow() {
+        return String.format("%d,%d,%d", this.size(), this.centroid().x(), this.centroid().y());
+    }
 }
--- src/GroupFinder.java
+++ src/GroupFinder.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class GroupFinder {
    /**
-    * Finds connected pixel groups of 1s in a binary image.
+    * Finds connected pixel groups of 1s in an integer array representing a binary image.
     * 
     * The input is a non-empty rectangular 2D array containing only 1s and 0s.
     * If the array or any of its subarrays are null, a NullPointerException
@@ -21,16 +21,17 @@ public class GroupFinder {
     * coordinates of the pixels in the group divided by the number of pixels in that group.
     * Similarly, the y coordinate of the centroid is the sum of all the y
     * coordinates of the pixels in the group divided by the number of pixels in that group.
+    * The division should be done as INTEGER DIVISION.
     *
     * The groups are sorted in DESCENDING order according to Group's compareTo method
-    * (size first, then y, then x). That is, the largest group will be first, the 
+    * (size first, then x, then y). That is, the largest group will be first, the 
     * smallest group will be last, and ties will be broken first by descending 
     * y value, then descending x value.
     * 
     * @param image a rectangular 2D array containing only 1s and 0s
     * @return the found groups of connected pixels in descending order
     */
-   public static List<Group> findCentroids(int[][] image) {
+   public static List<Group> findConnectedGroups(int[][] image) {
       return null;
    }
 }
