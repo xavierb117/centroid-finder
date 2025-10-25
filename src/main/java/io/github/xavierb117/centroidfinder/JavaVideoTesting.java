0871476cf0564b2fd8c46ef84f8cb912135d4ecc
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-10-24 21:02:43 -0700

    testing the import

--- src/main/java/io/github/xavierb117/centroidfinder/JavaVideoTesting.java
+++ src/main/java/io/github/xavierb117/centroidfinder/JavaVideoTesting.java
@@ -1,5 +1,14 @@
 package io.github.xavierb117.centroidfinder;
 
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
 public class JavaVideoTesting {
     public static void main(String[] args) {
         
