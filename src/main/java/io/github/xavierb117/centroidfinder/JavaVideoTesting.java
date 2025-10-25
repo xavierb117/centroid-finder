ba5d12709e6f75400d87f5d1770357be9c1a09f7
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-24 21:16:03 -0700

    Build failed, will fix

--- src/main/java/io/github/xavierb117/centroidfinder/JavaVideoTesting.java
+++ src/main/java/io/github/xavierb117/centroidfinder/JavaVideoTesting.java
@@ -11,6 +11,30 @@ import java.io.IOException;
 
 public class JavaVideoTesting {
     public static void main(String[] args) {
-        
+        String inputPath = "C:\\Users\\izeco\\OneDrive\\Desktop\\SDEV 334\\centroid-finder\\centroid-finder\\sampleInput\\ensantina.mp4";
+        String outputPath = "C:\\Users\\izeco\\OneDrive\\Desktop\\SDEV 334\\centroid-finder\\centroid-finder\\sampleOutput";
+
+        FFmpegFrameGrabber grab = new FFmpegFrameGrabber(inputPath);
+        try {
+            grab.start();
+
+            int frameToGrab = 0;
+            grab.setFrameNumber(frameToGrab);
+            Frame frame = grab.grabImage();
+            grab.close();
+
+            if (frame != null) {
+                Java2DFrameConverter convert = new Java2DFrameConverter();
+                BufferedImage bufferedImage = convert.convert(frame);
+                convert.close();
+
+                ImageIO.write(bufferedImage, "jpg", new File(outputPath));
+            }
+            else {
+                System.out.println("Couldn't grab the frame");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
