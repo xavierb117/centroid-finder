8f97a28af1a7f7dba2a20bc364bc7a60e2a735ee
Tim <timmywilliams4665@gmail.com>
2025-10-29 21:35:57 -0700

    Added test to confirm functionaility

--- src/test/java/io/github/xavierb117/centroidfinder/FrameGrabberTest.java
+++ src/test/java/io/github/xavierb117/centroidfinder/FrameGrabberTest.java
@@ -31,4 +31,21 @@ public class FrameGrabberTest {
         assertEquals(-1, result.get(0).centroid().y());
     }
 
+    @Test
+    void RecordsOneElementCorrectly() {
+        FrameGrabber grabber = new FrameGrabber(0xFF0000, 30, "fake.mp4") {
+            @Override
+            public List<TimeCoordinate> analysis() {
+                return List.of(new TimeCoordinate(0, new Coordinate(200, 150)));
+            }
+        };
+
+        List<TimeCoordinate> result = grabber.analysis();
+        assertEquals(1, result.size());
+        assertEquals(200, result.get(0).centroid().x());
+        assertEquals(150, result.get(0).centroid().y());
+    }
+
+    
+
 }
