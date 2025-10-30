0c6d8251badcfaa12be887d8ca7f24f111f77db0
Tim <timmywilliams4665@gmail.com>
2025-10-29 20:44:59 -0700

    Finished VideoResultWriter

--- src/main/java/io/github/xavierb117/centroidfinder/VideoResultWriter.java
+++ src/main/java/io/github/xavierb117/centroidfinder/VideoResultWriter.java
@@ -1,5 +1,25 @@
 package io.github.xavierb117.centroidfinder;
 
-public class VideoResultWriter {
-    
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class VideoResultWriter implements AutoCloseable {
+    private final PrintWriter writer;
+
+    public VideoResultWriter(String path) throws Exception {
+        writer = new PrintWriter(new FileWriter(path));
+        writer.println("time_seconds,x,y");
+    }
+
+    public void write(int second, Coordinate centroid)
+    {
+        writer.printf("%d,%d,%d%n", second, centroid.x(), centroid.y());
+    }
+
+    @Override
+    public void close()
+    {
+        writer.close();
+    }
+
 }
