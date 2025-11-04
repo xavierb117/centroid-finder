23298198a0c1f7f1ca7bb0fda546cf2056abe106
timmyd4 <timmywilliams4665@gmail.com>
2025-11-04 10:22:06 -0800

    added code for main

---
+++ server/main.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
