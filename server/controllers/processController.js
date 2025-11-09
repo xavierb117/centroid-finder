cf55dbb9f6af0afc849ca206fcfd7290d4d5b45e
Tim <timmywilliams4665@gmail.com>
2025-11-08 23:27:54 -0800

    AI USED error checks for invalid entry, and variables for file directories

--- server/controllers/processController.js
+++ server/controllers/processController.js
@@ -1,7 +1,23 @@
-export const startProcess = (req, res) => {
+import {spawn} from "child_process";
+import path from "path";
+import { randomUUID } from "crypto";
 
+const jobs = {};
+
+export const startProcess = (req, res) => {
+    const {filename} = req.params;
+    const {targetColor, threshold} = req.query;
+
+    if(!targetColor || !threshold)
+    {
+        return res.status(400).json({error: "Need TargetColor and or Threshold"})
+    }
+
+    const jobId = randomUUID();
+    const input = path.join(process.cwd(), "..", "processor", "videos", filename)
+    const output = path.join(process.cwd(), "..", "processor", "sampleOutput", `${filename}.csv`)
 }
 
 export const getProcess = (req, res) => {
-    
+
 }
