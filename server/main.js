e1b9e21cfe967999e4e5c4c0e3cc5eec6afe4e99
Tim <timmywilliams4665@gmail.com>
2025-11-08 23:39:06 -0800

    Added processRouter to main

--- server/main.js
+++ server/main.js
@@ -1,12 +1,14 @@
 import express from "express";
 import VideoRouter from "./routes/videosRoute.js";
 import thumbnailRouter from "./routes/thumbnailRoute.js"
+import processRouter from "./routes/processRoute.js"
 
 const app = express();
 app.use(express.json());
 
 app.use("/", VideoRouter);
-app.use("/", thumbnailRouter)
+app.use("/", thumbnailRouter);
+app.use("/", processRouter);
 
 const PORT = 3000;
 
