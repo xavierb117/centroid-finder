9004a4f807bea0915cfee22dc2dce8566a5c1afb
Tim <timmywilliams4665@gmail.com>
2025-11-09 20:54:01 -0800

    exporting the main to use with unit testing

--- server/main.js
+++ server/main.js
@@ -12,4 +12,9 @@ app.use("/", processRouter);
 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+if(process.env.NODE_ENV !== "test")
+{
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}
+
+export default app;
