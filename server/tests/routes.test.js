b9404bccd22f3195d448503184c31848577ff252
Tim <timmywilliams4665@gmail.com>
2025-11-09 21:07:22 -0800

    utilized jest, supertest and babel to get tests working

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -3,7 +3,7 @@ import app from "../main.js";
 
 describe("Express API routes", () => {
     test("GET /Videos should return a list of videos available to use", async () => {
-        const res = await request(app).get("/videos");
+        const res = await request(app).get("/api/videos");
         expect(res.status).toBe(200);
         expect(Array.isArray(res.body)).toBe(true);
     })
