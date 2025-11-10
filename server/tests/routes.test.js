9548e938c5ba139f5eb5f3c49884c88efebc6234
Tim <timmywilliams4665@gmail.com>
2025-11-09 21:29:53 -0800

    added another test for error checking on videoController

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -1,10 +1,30 @@
 import  request  from "supertest";
 import app from "../main.js";
 
+/*
+supertest is a JS libary
+Jest is the testing framework
+
+    #describe = groups together similar or related tests
+
+    #test = defines individual tests to run
+        #utilizes a asynchronous function
+        #makes a call to the main app
+        #test the endpoint of the route
+
+    #expect = makes assertions about what should be returned or the result 
+        #toBe = small assertion
+*/
+
 describe("Express API routes", () => {
     test("GET /Videos should return a list of videos available to use", async () => {
         const res = await request(app).get("/api/videos");
         expect(res.status).toBe(200);
         expect(Array.isArray(res.body)).toBe(true);
+    })
+
+    test("GET /Videos should return error", async () => {
+        const res = await request(app).get("/videos");
+        expect(res.status).toBe(404);
     })
 })
