defe9dc769adf72e9c4fba01aa7fa8f91cdf819b
Tim <timmywilliams4665@gmail.com>
2025-11-24 20:14:29 -0800

    Added jsdoc for /api/videos file

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -1,6 +1,14 @@
 import fs from "fs";
 import path from "path";
 
+/**
+ * Read a directory and return the results
+ * 
+ * @param req Express Request
+ * @param res Express Response
+ * @const dir passes a path from the .env file to the interal input (Videos) file
+ */
+
 export const video =  (req, res) => {
     const dir = process.env.VIDEOS
     fs.readdir(dir, (err, files) => {
