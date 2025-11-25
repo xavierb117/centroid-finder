79681111c18877cae9658db069e17da0b69bfaca
Tim <timmywilliams4665@gmail.com>
2025-11-24 20:54:44 -0800

    Added jsdoc for thumbnail and videos route

--- server/routes/thumbnailRoute.js
+++ server/routes/thumbnailRoute.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import {thumbnail} from '../controllers/thumbnailController.js'
 
+/**
+ * Mount :filename to process the 1st frame of the video
+ */
+
 const router = express.Router();
 
 router.get("/thumbnail/:filename", thumbnail)
--- server/routes/videosRoute.js
+++ server/routes/videosRoute.js
@@ -1,6 +1,10 @@
 import express from "express";
 import {video} from '../controllers/videoController.js'
 
+/**
+ * Mount videos to return the list of videos within the given directory
+ */
+
 const router = express.Router();
 
 router.get("/api/videos", video)
